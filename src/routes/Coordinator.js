180e1600501857e5069f3574490f1ec84f30c8d0
Everton Guedes <evertonguedes1@gmail.com>
2022-03-16 22:10:27 -0300

    paginação router

---
+++ src/routes/Coordinator.js
@@ -0,0 +1,12 @@
+export const goToPokemonsList = (history) => {
+    history.push("/")
+}
+
+export const goToPokemonDetail = (history) => {
+    history.push(`/pokemon/${name}`)
+}
+
+export const goToPokedex = (history) => {
+    history.push("/pokedex")
+}
+
